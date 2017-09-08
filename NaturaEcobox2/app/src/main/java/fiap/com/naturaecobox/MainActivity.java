bf00f27c75266ffd29c057bb0a8f947680e8cc1b
carlos <cmaraslian@hotmail.com>
2017-09-08 18:16:50 -0300

    Olha o mostuito aeww

--- NaturaEcobox2/app/src/main/java/fiap/com/naturaecobox/MainActivity.java
+++ NaturaEcobox2/app/src/main/java/fiap/com/naturaecobox/MainActivity.java
@@ -5,19 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnComprar;
-    private Button btnReciclar;
+    private ImageView btnComprar;
+    private ImageView btnReciclar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnComprar = (Button) findViewById(R.id.btnComprar);
-        btnReciclar = (Button) findViewById(R.id.btnReciclar);
+        btnComprar = (ImageView) findViewById(R.id.btnComprar);
+        btnReciclar = (ImageView) findViewById(R.id.btnReciclar);
     }
 
     public void comprar (View v){
